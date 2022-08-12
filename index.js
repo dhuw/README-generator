19a4c31169874c758a5811346fd730fb7ad9176e
dhuw <hazlewood.dyl@gmail.com>
2022-08-11 21:33:49 -0700

    starting indexjs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 // TODO: Create an array of questions for user input
+const generateMarkdown = require("./utils/generateMarkdown")
+const licenseBadge = require("./utils/licenseBadge")
 const questions = [];
 
 // TODO: Create a function to write README file
